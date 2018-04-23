d7e5e2a505bd47d1bb0e8780b1f4359b21c033b2
Anna <anna0706@gmx.at>
2018-04-23 12:06:42 +0200

    Basic 1 & 2

---
+++ basic2.js
@@ -0,0 +1,11 @@
+function ageCalculator(birth_year, current_year) {
+	var birth_year = 1989;
+	var current_year = 2018;
+
+	var age = current_year - birth_year; 
+	var age1 = age + 1; 
+
+	return age; 
+}
+
+document.write(You are either + "age" + or + "age1"); 
