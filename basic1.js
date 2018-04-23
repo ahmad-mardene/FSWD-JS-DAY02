28018017c35e3927e0d960d5bdb7b20418ff9942
ahmad-mardene <[ahmad.yoness@gmail.com]>
2018-04-23 13:18:58 +0200

    add basic 1.html

---
+++ basic1.js
@@ -0,0 +1,4 @@
+function crystalGazer ( job_title , location, partners_name, number_childrens ) {
+	document.write ( " you will be " + job_title + " in " + location + " and married to " + partners_name + " with " number_childrens );
+}
+crystalGazer( "doctor" , "wien" , "sara" , 5 );
---
+++ basic4.js
@@ -0,0 +1,6 @@
+function radianExchanger (degrees){
+	var result = degrees* (3.14/180) ;
+	document.write( result + " radians");
+
+}
+radianExchanger(90);
